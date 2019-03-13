f33efa2a601319f9135e1e54acdcf6af783dedd4
NAGAPUR NITHIN REDDY <nagapur@icloud.com>
2019-03-13 15:25:17 +0530

    Update Power_Bill.py

--- Power_Bill.py
+++ Power_Bill.py
@@ -1,7 +1,7 @@
 x = int(input("what is cost of current : "))
 y = int(input("Enter Number of units used : "))
 z = x/y
-print(z)
+print("Each unit is charged as : ",z)
 meter1 = int(input("First floor number of units used  :"))
 meter2 = int(input("Second floor number of units used :"))
 First_floor = meter1*z
